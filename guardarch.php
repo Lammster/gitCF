184cbbd8dedd435f1e8766db360c4bb1ff1c6206
LAMSTER <lamster1812@gmail.com>
2016-02-19 15:20:14 -0600

    se agrego vorder a los botons y a un campo

--- guardarch.php
+++ guardarch.php
@@ -10,6 +10,15 @@
 	<input type="text" name="nombre" /> <br/> <br/>
 	<textarea name="comentario1"></textarea> <br/>
 	<textarea name="comentario2"></textarea> <br/>
+	<style type="text/css">
+		input {border: 1px solid red; //Color del borde
+		background-color: yellow; //Color del fondo
+		color: white; //Color de letra
+		font-family: "Verdana, Arial"; //Tipo de letra
+		font-size: 15px;} //Medida de letra
+	</style>
+
+
 	<input type="submit" value="guardar dats" />
 	<input type="submit" value="my crazy Life" />
 </form>
