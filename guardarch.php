f67b1fb4dd0a64cf3ba7e122e7670ba48a1b01b5
LAMSTER <lamster1812@gmail.com>
2016-02-19 14:37:01 -0600

    agregamos el comit inicial

---
+++ guardarch.php
@@ -0,0 +1,16 @@
+<HTML>
+<HEAD>
+<META http-equiv="content-type" content="text/html; charset=utf-8"/>
+<title>Documento sin titulo</title>
+</HEAD>
+
+<BODY>
+<form action="guar.php" method="post" name="frm">
+	
+	<input type="text" name="nombre" /> <br/> <br/>
+	<textarea name="comentario"></textarea> <br/>
+	<input type="submit" value="guardar dats" />
+</form>
+
+</BODY>
+</HTML>
